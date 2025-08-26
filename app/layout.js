c97750dbdff120e50c1564c6ec115d06cd9088d8
Tushar7105 <tushar070105@gmail.com>
2025-08-26 19:53:17 +0530

    add authentication and schema for user

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,14 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {Outfit} from "next/font/google"
+import Provider from "./provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
+const outfit = Outfit({subsets:['latin']});
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
@@ -18,12 +21,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={outfit.className}
       >
-        {children}
+        <Provider>{children}</Provider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
