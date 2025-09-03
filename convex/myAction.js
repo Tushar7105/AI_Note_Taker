a6bf4a70e45261467178340145881ea8d10b64e7
Tushar7105 <tushar070105@gmail.com>
2025-09-03 23:40:35 +0530

    fix env issue

---
+++ convex/myAction.js
@@ -0,0 +1,49 @@
+"use node"
+
+import 'dotenv/config'
+import { ConvexVectorStore } from "@langchain/community/vectorstores/convex";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { TaskType } from "@google/generative-ai";
+import { action } from "./_generated/server.js";
+import { v } from 'convex/values';
+
+export const ingest = action({
+  args: {
+    splitText : v.any(),
+    fileId:v.string()
+  },
+  handler: async (ctx, args) => {
+    await ConvexVectorStore.fromTexts(
+      args.splitText,
+      args.splitText.map(() => ({ fileId: args.fileId })),
+      new GoogleGenerativeAIEmbeddings({
+        apiKey : process.env.NEXT_PUBLIC_GEMINI_API_KEY,
+        model: "text-embedding-004", 
+        taskType: TaskType.RETRIEVAL_DOCUMENT,
+        title: "Document title",
+      }),
+      { ctx }
+    );
+    return "Completed...."
+  },
+});
+
+export const search = action({
+  args: {
+    query: v.string(),
+    fileId : v.string()
+  },
+  handler: async (ctx, args) => {
+    const vectorStore = new ConvexVectorStore(new GoogleGenerativeAIEmbeddings({
+        apiKey : 'AIzaSyCXUBgUmiHPpmTA_JNKBsjOrqpFer3Wp4w',
+        model: "text-embedding-004", 
+        taskType: TaskType.RETRIEVAL_DOCUMENT,
+        title: "Document title",
+      }), { ctx });
+
+    const resultOne = await (await vectorStore.similaritySearch(args.query, 1))
+                      .filter(q=>q.metadata.fileId==args.fileId);
+    console.log(resultOne);
+    return JSON.stringify(resultOne);
+  },
+});
